af9df47b19f4703bd7bb0f375b8a99a4578b17d2
CaMocBaJL <gorka19800@gmail.com>
2021-03-28 12:02:56 +0400

    no message

---
+++ Task 2.2/Game/Collectables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    abstract class Collectables
+    {
+        public abstract ConsoleColor Color { get; set; }
+    }
+}
