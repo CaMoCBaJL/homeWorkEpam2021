55b2a8a75ad943c5c7afdd9c22fae33095a087e7
CaMoCBaJL <gorka19800@gmail.com>
2021-04-13 19:22:19 +0400

    Task 3.3.3 is complete!
    It consist of 4 classes:
    
    -static class Order is incapsulating logic to order some pizza:
    
     MakeOrder method modulates process of pizza purchasing.
    GetOrderId calculates the ID of current order.
    
    -Pizzeria contains methods to act with user:
    
    MakeOrder method indicates the begging of order fulfilment:
    TakeOrder method indicates that certain User's order is ready.
    
    -User is a model of Pizzeria Client. He has receipt with Id & list of purchased pizzas

---
+++ Task 3.3/3.3.3/ConsoleApp1/ConsoleApp1/Pizzeria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Pizzeria
+    {
+        public event Action<User, string> NotifyUser;
+
+        public void MakeOrder (User usr)
+        {
+            if (NotifyUser != null)
+            {
+                if (usr.OrderId != int.MinValue)
+                    NotifyUser.Invoke(usr, $"Заказ {usr.OrderId} начали готовить! ");
+                else
+                    NotifyUser.Invoke(usr, $"Эй, {usr.UserName} закажи уже чего-нибудь!");
+            }
+            else
+                Console.WriteLine("Пиццерия закрыта!");
+        }
+
+        public void TakeOrder(User usr)
+        {
+            if (NotifyUser != null)
+            {
+                if (usr.OrderId != int.MinValue)
+                    NotifyUser.Invoke(usr, $"Заказ {usr.OrderId} готов! ");
+            }
+            else
+                Console.WriteLine("Пиццерия закрыта!");
+        }
+
+
+
+    }
+}
