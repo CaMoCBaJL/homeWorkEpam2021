3640c8f11c845d6b0b4bd4755df1eb3f352084de
CaMoCBaJL <gorka19800@gmail.com>
2021-04-18 17:31:23 +0400

    Started Task 4.1
    Created Base structure of project.

---
+++ Task 4.1/MyGit/MyGit/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGit
+{
+    static class Menu
+    {
+        public static void ChooseMode()
+        {
+            Console.WriteLine($"Welcome back, {Environment.UserName}!");
+
+            int mode = -1;
+
+            while(true)
+            {
+                Console.WriteLine("Choose mode:");
+
+                Console.WriteLine("1. Viewer mode.");
+
+                Console.WriteLine("2. Rollback mode.");
+
+
+                if (int.TryParse(Console.ReadLine(), out mode))
+                {
+                    switch (mode)
+                    {
+                        case 1:
+                        case 2:
+                            MySVC.InitializeMySVC(mode);
+                            return;
+
+                        default:
+                            Console.WriteLine("Incorrect input!");
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine("Incorrect input!");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Task 4.1/MyGit/MyGit/MySVC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MyGit
+{
+    static class MySVC
+    {
+        private static readonly string dirPath = "./CurDir";
+
+        public static void InitializeMySVC(int mode)
+        {
+
+            switch (mode)
+            {
+                case 1:
+                    Console.WriteLine("Viewer mode enabled.");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Rollback mode enabled.");
+                    break;
+            }
+
+            try
+            {
+                if (!Directory.Exists(dirPath))
+                    Directory.CreateDirectory(dirPath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to create working directory. See:" +Environment.NewLine + ex.Message);
+            }
+        }
+
+        public static void CloseMySVC()
+        {
+
+        }
+
+    }
+}
