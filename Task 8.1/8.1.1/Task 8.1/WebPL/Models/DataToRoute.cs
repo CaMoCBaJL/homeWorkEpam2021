37b742df195d1e176a087861d4aa8680918791ca
DESKTOP-VHEEB1U\gorka <gorka19800@gmail.com>
2021-06-25 22:33:12 +0300

    Added page with entities lists.

---
+++ Task 8.1/8.1.1/Task 8.1/WebPL/Models/DataToRoute.cs
@@ -0,0 +1,10 @@
+﻿using Entities;
+using System.Collections.Generic;
+
+namespace WebPL.Models
+{
+    public static class DataToRoute
+    {
+        public static List<string> Data { get; set; }
+    }
+}
